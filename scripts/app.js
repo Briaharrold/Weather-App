961232dd09e49a7eac268fc20ebfdffb4d268760
Briaharrold <94560548+Briaharrold@users.noreply.github.com>
2023-12-04 14:55:05 -0800

    first comit

---
+++ scripts/app.js
@@ -0,0 +1,126 @@
+import{apiKey} from "./environment.js"
+let usersInput = document.getElementById("usersInput");
+let btn = document.getElementById("btn");
+let insertHere = document.getElementById("Insert Here");
+let insertMin = document.getElementById("Insert Min");
+let insertMax = document.getElementById("Insert Max");
+let insertName = document.getElementById("Insert Name");
+let insertDay = document.getElementById("Insert Day");
+const d = Date();
+const weekDay=["Sunday","Monday","Tuesday","Wednesday",,"Thursday","Friday","Saturday" ];
+
+function ApiCall(){
+    //We are goin to initaiate the fetcher request from our random wor api
+    // fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockton&appid=a843da14ff8561f5d288b3a4dc783df6')
+  
+  
+
+    let userData = usersInput.value;
+    console.log(userData);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userData}&appid=${apiKey}&units=imperial`)
+
+    .then((response)=>{
+        //using JSON method to parse into json data
+        return response.json();
+    })
+    // .then((data=>  console.log(data[0])))
+
+    //This is getting the data from the api  and inserting the degrees onto the html page
+    //The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number.
+    .then((data => insertHere.innerHTML =  Math.floor(data.main.feels_like)+ "°"))
+    
+    // console.log(Math.floor(data.main.feels_like));
+
+    //THIS GETS WEATHER[] DESCRIPTION
+    // .then((data =>  insertHere.innerHTML = data.weather[0].main))
+
+    //GETS LOWEST DEGREE
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userData}&appid=${apiKey}&units=imperial`)
+    .then((response)=>{
+        //using JSON method to parse into json data
+        return response.json();
+    })
+    .then((data=> insertMin.innerHTML =  " Min." + Math.floor(data.main.temp_min)+ "°"))
+    //GETS HIGHEST DEGREE
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userData}&appid=${apiKey}&units=imperial`)
+    .then((response)=>{
+       
+        return response.json();
+    })
+    .then((data=> insertMax.innerHTML = " Max." + Math.floor(data.main.temp_max)+ "°"))
+//GETS NAME OF CITY
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userData}&appid=${apiKey}&units=imperial`)
+    .then((response)=>{
+       
+        return response.json();
+    })
+    .then((data=> insertName.innerHTML = data.name))
+
+    // fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${apiKey}&units=imperial`)
+    // .then((response)=>{
+       
+    //     return response.json();
+    // })
+    // .then((data=> insertDay.innerHTML = data.list[6].dt_txt))
+fetch(`https://history.openweathermap.org/data/3.0/history/timemachine?lat=51.51&lon=-0.13&dt=606348800&appid=${apiKey}`)
+.then((response)=>{
+       
+    return response.json();
+})
+.then((data=> insertDay.innerHTML = data.dt))
+
+    
+}
+
+btn.addEventListener('click',function(e){
+//on click we are going to make data pop up based on what city user picks
+apiCall()
+ApiCall();
+});
+//navigator.geolocation returns a gelocation object lat and long
+// getCurrentPosition() Allows if the user allows us to access their loc run first functio if not run error function
+navigator.geolocation.getCurrentPosition(success, errorFunc);
+{
+    coords:{
+        latitude: 32.1234;
+        longitude:13.1234;
+    }
+}
+function success(position){
+    console.log("Our latitude " + position.coords.latitude);
+   console.log("Our longitude "+ position.coords.longitude);
+   let gotPosition = function (position) {
+
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    console.log(long) ;
+    console.log(lat) ;
+   }
+   
+   fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}`)
+   
+     .then((response)=>{
+           //using JSON method to parse into json data
+           return response.json();
+       })
+       // .then((data=>  console.log(data[0])))
+   
+       //This is getting the data from the api  and inserting the degrees onto the html page
+       //The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number.
+       .then((data => insertHere.innerHTML =  Math.floor(data.main.feels_like)+ "°"))
+   }
+function errorFunc(error){
+console.log(error.message);
+}
+
+
+async function apiCall(){
+ 
+    let lat = success(position.coords.latitude);
+    let long =  success(position.coords.longitude);
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}`);
+    const data = await promise.json();
+    console.log(data.weather[3]);
+    
+
+}
---
+++ scripts/environment.js
@@ -0,0 +1,2 @@
+export {apiKey}
+let apiKey= "a843da14ff8561f5d288b3a4dc783df6";
