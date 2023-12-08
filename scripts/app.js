c70a155d9115e8ca5e5eef9ea6f48e9a1e786018
Briaharrold <94560548+Briaharrold@users.noreply.github.com>
2023-12-07 16:50:01 -0800

    SAD

--- scripts/app.js
+++ scripts/app.js
@@ -6,13 +6,12 @@ let insertMin = document.getElementById("Insert Min");
 let insertMax = document.getElementById("Insert Max");
 let insertName = document.getElementById("Insert Name");
 let insertDay = document.getElementById("Insert Day");
+let insertType = document.getElementById("Insert Type");
 const d = Date();
-let date2 = document.getElementById("date");
 const weekDay=["Sunday","Monday","Tuesday","Wednesday",,"Thursday","Friday","Saturday" ];
 let lat;
 let long;
-let t = new Date();
-document.getElementById('date').innerText = t.toDateString();
+
 function ApiCall(){
     //We are goin to initaiate the fetcher request from our random wor api
     // fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockton&appid=a843da14ff8561f5d288b3a4dc783df6')
@@ -33,16 +32,10 @@ function ApiCall(){
         //using JSON method to parse into json data
         return response.json();
     })
-    // .then((data=>  console.log(data[0])))
 
-    //This is getting the data from the api  and inserting the degrees onto the html page
-    //The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number.
     .then((data => insertHere.innerHTML =  Math.floor(data.main.feels_like)+ "°"))
     
-    // console.log(Math.floor(data.main.feels_like));
-
-    //THIS GETS WEATHER[] DESCRIPTION
-    // .then((data =>  insertHere.innerHTML = data.weather[0].main))
+  
 
     //GETS LOWEST DEGREE
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userData}&appid=${apiKey}&units=imperial`)
@@ -79,11 +72,19 @@ fetch(`https://history.openweathermap.org/data/3.0/history/timemachine?lat=51.51
 })
 .then((data=> insertDay.innerHTML = data.dt))
 
-    
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=stockton&appid=${apiKey}&units=imperial`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data => insertType.innerHTML = " Type " + data.weather[0].main))
+     
+ 
 }
 
 btn.addEventListener('click',function(e){
-    
+    let t = new Date();
+document.getElementById('date').innerText = t.toDateString();
 //on click we are going to make data pop up based on what city user picks
 ApiCall();
 });
@@ -119,6 +120,7 @@ function success(position){
        .then((data => insertHere.innerHTML =  Math.floor(data.main.feels_like)+ "°"))
        
    }
+   
 function errorFunc(error){
 console.log(error.message);
 }
